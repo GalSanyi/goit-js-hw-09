b232363cbac054ed8fd1635afef0e706d26bfb4b
GalSanyi <galsanyi84@gmail.com>
2021-12-09 21:36:03 +0100

    great

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,12 +6,12 @@ import "flatpickr/dist/flatpickr.min.css";
 
 
 const refs = {
-    input: document.querySelector('input[type="text"]'),
-    button: document.querySelector('button[data-start]'),
+    inputData: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('button[data-start]'),
     days: document.querySelector('[data-days]'),
     hours: document.querySelector('[data-hours]'),
     minutes: document.querySelector('[data-minutes]'),
-seconds: document.querySelector('[data-seconds]'),
+    seconds: document.querySelector('[data-seconds]'),
 
 
 };
@@ -27,75 +27,89 @@ const options = {
     onClose(selectedDates) {
 
         // console.log(selectedDates[0]);
-        if (selectedDates[0] < new Date()) {
-            refs.button.disabled = true;
-
-
-            window.alert("Please choose a date in the future");
+        if (selectedDates[0] < Date.now()) {
+            refs.startBtn.setAttribute('disabled', true);
+            window.alert("Please choose a date in the future")
         }
-        
-        if (selectedDates[0] > new Date()) {
+        refs.startBtn.removeAttribute('disabled');
 
-            refs.button.disabled = false;
-        }
+        refs.inputData.setAttribute('disabled', true);
+        refs.startBtn.addEventListener('click', () => {
+            function timer() {
+                let currentData = new Date();
+                const dataTime = selectedDates[0] - currentData;
+                if (dataTime < 0) {
+                    return;
+                }
+
+                let time = convertMs(dataTime);
+
+                console.log(time)
+
+                updateTimer(time)
+
+
+            }
+            setInterval(timer, 1000);
+
+
+        })
     },
 };
 flatpickr('#datetime-picker', options);
 
-refs.button.addEventListener('click', onClickTimer);
 
 
 
-const startTime = Date.now();
+
+// const startTime = Date.now();
 
 
-function onClickTimer() {
-    setInterval(() => {
-const currentTime = Date.now();
-const deltaTime = startTime - currentTime;
-const  time = convertMs(deltaTime);
-updateTimer(time);
+// function onClickTimer() {
+//     setInterval(() => {
+//         let currentTime = Date.now();
+//         const dataTime = startTime - currentTime;
 
-        
-    },1000); 
-}
+//         if (dataTime < 0) {
+//             return;
+//         }
+//         let time = convertMs(dataTime);
+//         updateTimer(time);
+
+
+//     }, 1000);
+// }
 
 function updateTimer({ days, hours, minutes, seconds }) {
     refs.days.textContent = `${days}`;
     refs.hours.textContent = `${hours}`;
     refs.minutes.textContent = `${minutes}`;
     refs.seconds.textContent = `${seconds}`;
-   
+
 }
 
 
 
-function addLeadingZero(value){
+function addLeadingZero(value) {
     return String(value).padStart(2, '0');
 }
 
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
+    return { days, hours, minutes, seconds };
 }
-
-
-
-
-
-
