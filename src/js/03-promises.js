095126a3db0db5c81a9890b55814555aca15f327
GalSanyi <galsanyi84@gmail.com>
2021-12-10 21:16:25 +0100

    update-hw-9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+    form: document.querySelector('.form'),
+    inputDelay: document.querySelector('input[name="delay"]'),
+    inpuStep: document.querySelector('input[name="step"]'),
+    inputAmount: document.querySelector('input[name="amount"]'),
+}
+
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+    e.preventDefault();
+
+    let formData = new FormData([form]);
+    console.log(formData);
 }
 
 
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
+
+function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+        // Fulfill
+    } else {
+        // Reject
+    }
+}
+
+
+
+
+
+// createPromise(2, 1500)
+//     .then(({ position, delay }) => {
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     })
