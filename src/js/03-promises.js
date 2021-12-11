b631aa60a083b0182fe59a6b534b43f60e1f56a7
GalSanyi <galsanyi84@gmail.com>
2021-12-11 17:24:25 +0100

    finished work

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,36 +1,63 @@
-const refs = {
-    form: document.querySelector('.form'),
-    inputDelay: document.querySelector('input[name="delay"]'),
-    inpuStep: document.querySelector('input[name="step"]'),
-    inputAmount: document.querySelector('input[name="amount"]'),
-}
+const form = document.querySelector('.form');
 
-refs.form.addEventListener('submit', onSubmitForm);
+
+form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(e) {
     e.preventDefault();
+    const formData = new FormData(form);
 
-    let formData = new FormData([form]);
-    console.log(formData);
-}
+    const user = {};
+    formData.forEach((value, name) => {
+        user[name] = value;
+    });
+
+    const amount = user.amount;
+    console.log(amount);
+
+    const delayUser = user.delay;
+    console.log(delayUser);
+
+    const stepUser = user.step;
+    console.log(stepUser);
+
+
+    let position = '';
+    let delay = Number(delayUser);
+    let step = Number(stepUser);
+    for (let i = 1; i <= amount; i += 1) {
+        position = i;
+
+
+        delay += step
 
 
 
+        const createPromise = (position, delay) => {
 
-function createPromise(position, delay) {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-        // Fulfill
-    } else {
-        // Reject
+            return new Promise((resolve, reject) => {
+                const shouldResolve = Math.random() > 0.3;
+
+                setInterval(() => {
+
+
+                    if (shouldResolve) {
+                        // Fulfill 
+                        resolve({ position, delay });
+                    }
+                    // Reject
+                    reject({ position, delay });
+
+                }, delay)
+            });
+        };
+        createPromise(2, 1500)
+            .then(({ position, delay }) => {
+                console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+            .catch(({ position, delay }) => {
+                console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+
     }
 }
-
-
-
-
-
-createPromise(2, 1500)
-    .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
