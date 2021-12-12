8d0fe0973f602765279cfe483cb6f6dc9b8e97f6
GalSanyi <galsanyi84@gmail.com>
2021-12-12 20:35:47 +0100

    timer

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.form');
 
 
@@ -51,13 +53,15 @@ function onSubmitForm(e) {
                 }, delay)
             });
         };
-        createPromise(2, 1500)
+        createPromise(position, delay)
             .then(({ position, delay }) => {
-                console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+                Notiflix.Notify.success(` Fulfilled promise ${position} in ${delay}ms`);
             })
             .catch(({ position, delay }) => {
-                console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+                Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
             });
+    };
 
-    }
-}
+
+
+};
